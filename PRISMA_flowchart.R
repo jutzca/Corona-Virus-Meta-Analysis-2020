1c9650f74e790f537fec7ef846c4571686b2fd56
Catherine Jutzeler <catherine.jutzeler@bsse.ethz.ch>
2020-03-29 12:48:56 +0200

    Update PRISMA_flowchart.R

--- PRISMA_flowchart.R
+++ PRISMA_flowchart.R
@@ -136,14 +136,17 @@ p <- p +
     size = 0.15, lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type = "closed"))
 
+
+#Create boxes for flowchart
+#Total results from search
 p <- p +
   geom_rect(xmin = 20, xmax = 50, ymin = 70, ymax = 80, color = 'black',
             fill = 'white', size = 0.25) +
   annotate('text', x = 35, y = 75, label = paste('Total results from search\n (n=',
                                                  format_statistics(total_results_from_search),
-                                                 ')'),
-           size = 4)
+                                                 ')'), size = 4)
 
+#Duplicates removed
 p <- p +
   geom_segment(
     x = 35, xend = 55, y = 67.5, yend = 67.5,
@@ -159,17 +162,15 @@ p <- p +
             fill = 'white', size = 0.25) +
   annotate('text', x = 70, y = 67.5, label = paste('Duplicates removed\n (n=',
                                                    format_statistics(duplicates_removed),
-                                                   ')'),
-           size = 4)
-
+                                                   ')'), size = 4)
+#Records screened
 p <- p +
   geom_rect(xmin = 20, xmax = 50, ymin = 55, ymax = 65, color = 'black',
             fill = 'white', size = 0.25) +
   annotate('text', x = 35, y = 60, label = paste('Records screened\n (n=',
                                                  format_statistics(records_screened),
-                                                 ')'),
-           size = 4)
-
+                                                 ')'),size = 4)
+#Articles excluded (Title or Abstract)
 p <- p +
   geom_segment(
     x = 35, xend = 55, y = 52.5, yend = 52.5,
@@ -185,17 +186,16 @@ p <- p +
             fill = 'white', size = 0.25) +
   annotate('text', x = 70, y = 52.5, label = paste('Articles excluded (Title or Abstract)\n (n=',
                                                    format_statistics(exclusion1),
-                                                   ')'),
-           size = 4)
-
+                                                   ')'),size = 4)
+#Full-text articles retrieved
 p <- p +
   geom_rect(xmin = 20, xmax = 50, ymin = 40, ymax = 50, color = 'black',
             fill = 'white', size = 0.25) +
   annotate('text', x = 35, y = 45, label = paste('Full-text articles retrieved\n (n=',
                                                  format_statistics(full_text_articles_retrieved),
-                                                 ')'),
-           size = 4)
+                                                 ')'), size = 4)
 
+#Articles excluded (Full-text)
 p <- p +
   geom_segment(
     x = 35, xend = 55, y = 37.5, yend = 37.5,
@@ -211,17 +211,16 @@ p <- p +
             fill = 'white', size = 0.25) +
   annotate('text', x = 70, y = 37.5, label = paste('Articles excluded (Full-text)\n (n=',
                                                    format_statistics(exclusion2),
-                                                   ')'),
-           size = 4)
+                                                   ')'),size = 4)
 
+#Articles included in the synthesis
 p <- p +
   geom_rect(xmin = 20, xmax = 50, ymin = 25, ymax = 35, color = 'black',
             fill = 'white', size = 0.25) +
   annotate('text', x = 35, y = 30, label = paste('Articles included in the synthesis\n (n=',
                                                  format_statistics(articles_indluded_in_synthesis),
-                                                 ')'),
-           size = 4)
-
+                                                 ')'),size = 4)
+#Articles included in the classification
 p <- p +
   geom_segment(
     x = 35, xend = 35, y = 25, yend = 20,
@@ -233,8 +232,7 @@ p <- p +
             fill = 'white', size = 0.25) +
   annotate('text', x = 35, y = 15, label = paste('Articles included in the classification\n (n=',
                                                  format_statistics(extraction),
-                                                 ')'),
-           size = 4)
+                                                 ')'),size = 4)
 
 #Define theme
 p <- p + theme_void()
