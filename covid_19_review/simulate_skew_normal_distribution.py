c332d0e70cb93752c4dd520cafd5edc77b901156
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-04-12 14:46:24 +0200

    Added proper parsing function for entries

--- covid_19_review/simulate_skew_normal_distribution.py
+++ covid_19_review/simulate_skew_normal_distribution.py
@@ -4,6 +4,9 @@
 # mean and standard deviation, respectively, and converts them to median
 # and IQR.
 
+import re
+
+import numpy as np
 import pandas as pd
 
 
@@ -32,6 +35,56 @@ def standard_approximation(mean, std):
     return median, iqr
 
 
+def extract_mean_and_std(entry):
+    """Extract mean and standard deviation from an entry.
+
+    Parses an entry in string format and tries to extract the mean and
+    standard deviation. The following formats are supported:
+
+    - `mean (+- std)`
+    - `mean (+/- std)`
+    - `mean (std)`
+
+    Parameters
+    ----------
+    entry : str
+        Entry from which to parse mean and standard deviation. If the
+        mean fails, a tuple of NaN values will be returned.
+
+    Returns
+    -------
+    Tuple of (`mean`, `std`). Can be NaN in case the parsing failed.
+    """
+    try:
+        # First split according to whitespace, such that next, we are
+        # dealing with two tuples.
+        mean, std = entry.split(maxsplit=1)
+    except ValueError:
+        return np.nan, np.nan
+
+    try:
+        # This should always work for valid entries; if not, there is
+        # nothing we can do here.
+        mean = float(mean)
+    except ValueError:
+        mean = np.nan
+
+    # This ensures that all valid formats are covered. If we are able to
+    # convert `std` into a float after removing the nuisance characters,
+    # we have a valid number.
+    std = std.replace('(', '')
+    std = std.replace(')', '')
+    std = std.replace('+-', '')
+    std = std.replace('+/-', '')
+
+    try:
+        std = float(std)
+    except ValueError:
+        std = np.nan
+
+    return mean, std
+
+
 if __name__ == '__main__':
     filename = '../data/data.xlsx'
 
@@ -43,3 +96,4 @@ if __name__ == '__main__':
         # This is the entry we are interested in. We try to parse it
         # afterwards and then continue with the simulation.
         age_reported = row['Age_reported']
+        mean, std = extract_mean_and_std(age_reported)
